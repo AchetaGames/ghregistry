7c68a17dba27eab7018706ac27ea866428c33618
Milan Stastny <milan@acheta.games>
2021-10-20 01:42:48 +0200

    Add initial implementation

---
+++ src/blobs.rs
@@ -0,0 +1,235 @@
+use crate::errors::{Error, Result};
+use crate::Client;
+use reqwest;
+use reqwest::{Method, StatusCode};
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+use std::sync::mpsc::Sender;
+
+impl Client {
+    /// Check if a blob exists.
+    pub fn has_blob(&self, name: &str, digest: &str) -> Result<bool> {
+        let url = {
+            let ep = format!("{}/v2/{}/blobs/{}", self.base_url, name, digest);
+            reqwest::Url::parse(&ep)?
+        };
+
+        let res = self.build_reqwest(Method::HEAD, url.clone()).send()?;
+
+        trace!("Blob HEAD status: {:?}", res.status());
+
+        match res.status() {
+            StatusCode::OK => Ok(true),
+            _ => Ok(false),
+        }
+    }
+
+    /// Retrieve blob.
+    pub fn get_blob(&self, name: &str, digest: &str) -> Result<Vec<u8>> {
+        let digest = crate::ContentDigest::try_new(digest.to_string())?;
+
+        let blob = {
+            let ep = format!("{}/v2/{}/blobs/{}", self.base_url, name, digest);
+            let url = reqwest::Url::parse(&ep)?;
+
+            let res = self.build_reqwest(Method::GET, url.clone()).send()?;
+
+            trace!("GET {} status: {}", res.url(), res.status());
+            let status = res.status();
+
+            // Let client errors through to populate them with the body
+            if !(status.is_success() || status.is_client_error()) {
+                return Err(Error::UnexpectedHttpStatus(status));
+            }
+
+            let status = res.status();
+            let body_vec = res.bytes()?.to_vec();
+            let len = body_vec.len();
+
+            if status.is_success() {
+                trace!("Successfully received blob with {} bytes ", len);
+                Ok(body_vec)
+            } else if status.is_client_error() {
+                Err(Error::Client {
+                    status,
+                    len,
+                    body: body_vec,
+                })
+            } else {
+                // We only want to handle success and client errors here
+                error!(
+                    "Received unexpected HTTP status '{}' after fetching the body. Please submit a bug report.",
+                    status
+                );
+                Err(Error::UnexpectedHttpStatus(status))
+            }
+        }?;
+
+        digest.try_verify(&blob)?;
+        Ok(blob.to_vec())
+    }
+
+    /// Retrieve blob with progress
+    pub fn get_blob_with_progress(
+        &self,
+        name: &str,
+        digest: &str,
+        sender: Option<Sender<u64>>,
+    ) -> Result<Vec<u8>> {
+        let digest = crate::ContentDigest::try_new(digest.to_string())?;
+
+        let blob = {
+            let ep = format!("{}/v2/{}/blobs/{}", self.base_url, name, digest);
+            let url = reqwest::Url::parse(&ep)?;
+
+            let mut res = self.build_reqwest(Method::GET, url.clone()).send()?;
+
+            trace!("GET {} status: {}", res.url(), res.status());
+            let status = res.status();
+            // Let client errors through to populate them with the body
+            if !(status.is_success() || status.is_client_error()) {
+                if let Some(send) = sender {
+                    drop(send);
+                };
+                return Err(Error::UnexpectedHttpStatus(status));
+            }
+
+            let status = res.status();
+
+            let mut buffer: [u8; 1024] = [0; 1024];
+            let mut body_vec: Vec<u8> = Vec::new();
+            loop {
+                match res.read(&mut buffer) {
+                    Ok(size) => {
+                        if size > 0 {
+                            if let Some(send) = &sender {
+                                send.send(size as u64).unwrap();
+                            };
+                            body_vec.append(&mut buffer[0..size].to_vec());
+                        } else {
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        error!("Download error: {:?}", e);
+                        break;
+                    }
+                }
+            }
+            let len = body_vec.len();
+
+            if let Some(send) = sender {
+                drop(send);
+            };
+            if status.is_success() {
+                trace!("Successfully received blob with {} bytes ", len);
+                Ok(body_vec)
+            } else if status.is_client_error() {
+                Err(Error::Client {
+                    status,
+                    len,
+                    body: body_vec,
+                })
+            } else {
+                // We only want to handle success and client errors here
+                error!(
+                    "Received unexpected HTTP status '{}' after fetching the body. Please submit a bug report.",
+                    status
+                );
+                Err(Error::UnexpectedHttpStatus(status))
+            }
+        }?;
+
+        digest.try_verify(&blob)?;
+        Ok(blob.to_vec())
+    }
+
+    /// Retrieve blob with progress
+    pub fn get_blob_with_progress_file(
+        &self,
+        name: &str,
+        hash: &str,
+        sender: Option<Sender<u64>>,
+        target_dir: &Path,
+    ) -> Result<PathBuf> {
+        let digest = crate::ContentDigest::try_new(hash.to_string())?;
+        let mut target = target_dir.to_path_buf();
+        std::fs::create_dir_all(&target).unwrap();
+        target.push(hash);
+        trace!("Going to downloaad to: {:?}", target);
+
+        let ep = format!("{}/v2/{}/blobs/{}", self.base_url, name, digest);
+        let url = reqwest::Url::parse(&ep)?;
+
+        let mut res = match self.build_reqwest(Method::GET, url.clone()).send() {
+            Ok(res) => res,
+            Err(e) => {
+                println!("Unable to create request: {:?}", e);
+                return Err(Error::DownloadFailed);
+            }
+        };
+
+        trace!("GET {} status: {}", res.url(), res.status());
+        let status = res.status();
+        // Let client errors through to populate them with the body
+        if !(status.is_success() || status.is_client_error()) {
+            if let Some(send) = sender {
+                drop(send);
+            };
+            return Err(Error::UnexpectedHttpStatus(status));
+        }
+
+        let status = res.status();
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(&target)
+            .unwrap();
+        let mut len: usize = 0;
+        let mut buffer: [u8; 1024] = [0; 1024];
+        loop {
+            match res.read(&mut buffer) {
+                Ok(size) => {
+                    if size > 0 {
+                        if let Some(send) = &sender {
+                            send.send(size as u64).unwrap();
+                        };
+                        len += size;
+                        file.write_all(&buffer[0..size]).unwrap();
+                    } else {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    error!("Download error: {:?}", e);
+                    break;
+                }
+            }
+        }
+
+        if let Some(send) = sender {
+            drop(send);
+        };
+        if status.is_success() {
+            trace!("Successfully received blob with {} bytes ", len);
+            // digest.try_verify(&blob)?;
+            Ok(target.clone())
+        } else if status.is_client_error() {
+            Err(Error::Client {
+                status,
+                len,
+                body: vec![],
+            })
+        } else {
+            // We only want to handle success and client errors here
+            error!(
+                    "Received unexpected HTTP status '{}' after fetching the body. Please submit a bug report.",
+                    status
+                );
+            Err(Error::UnexpectedHttpStatus(status))
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,180 @@
+use std::collections::HashMap;
+use std::io::Read;
+
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate strum_macros;
+
+pub mod mediatypes;
+
+mod config;
+mod errors;
+
+use errors::{Error, Result};
+use serde::{Deserialize, Serialize};
+
+// use crate::errors::*; use reqwest::{Method, StatusCode, Url};
+
+pub use crate::config::Config;
+
+// mod catalog;
+
+mod auth;
+mod tags;
+
+pub use auth::WwwHeaderParseError;
+
+pub mod manifest;
+
+mod blobs;
+
+mod content_digest;
+pub(crate) use self::content_digest::ContentDigest;
+pub use self::content_digest::ContentDigestError;
+
+pub static USER_AGENT: &str = "acheta-ghregistry/0.0";
+
+/// Get registry credentials from a JSON config reader.
+///
+/// This is a convenience decoder for docker-client credentials
+/// typically stored under `~/.docker/config.json`.
+pub fn get_credentials<T: Read>(
+    reader: T,
+    index: &str,
+) -> Result<(Option<String>, Option<String>)> {
+    let map: Auths = serde_json::from_reader(reader)?;
+    let real_index = match index {
+        // docker.io has some special casing in config.json
+        "docker.io" | "registry-1.docker.io" => "https://index.docker.io/v1/",
+        other => other,
+    };
+    let auth = match map.auths.get(real_index) {
+        Some(x) => base64::decode(x.auth.as_str())?,
+        None => return Err(Error::AuthInfoMissing(real_index.to_string())),
+    };
+    let s = String::from_utf8(auth)?;
+    let creds: Vec<&str> = s.splitn(2, ':').collect();
+    let up = match (creds.get(0), creds.get(1)) {
+        (Some(&""), Some(p)) => (None, Some(p.to_string())),
+        (Some(u), Some(&"")) => (Some(u.to_string()), None),
+        (Some(u), Some(p)) => (Some(u.to_string()), Some(p.to_string())),
+        (_, _) => (None, None),
+    };
+    trace!("Found credentials for user={:?} on {}", up.0, index);
+    Ok(up)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Auths {
+    auths: HashMap<String, AuthObj>,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct AuthObj {
+    auth: String,
+}
+
+/// A Client to make outgoing API requests to a registry.
+#[derive(Clone, Debug)]
+pub struct Client {
+    base_url: String,
+    credentials: Option<(String, String)>,
+    index: String,
+    user_agent: Option<String>,
+    auth: Option<auth::Auth>,
+    client: reqwest::blocking::Client,
+}
+
+impl Client {
+    pub fn configure() -> Config {
+        Config::default()
+    }
+
+    //     /// Ensure remote registry supports v2 API.
+    //     pub async fn ensure_v2_registry(self) -> Result<Self> {
+    //         if !self.is_v2_supported().await? {
+    //             Err(Error::V2NotSupported)
+    //         } else {
+    //             Ok(self)
+    //         }
+    //     }
+    //
+    //     /// Check whether remote registry supports v2 API.
+    //     pub async fn is_v2_supported(&self) -> Result<bool> {
+    //         match self.is_v2_supported_and_authorized().await {
+    //             Ok((v2_supported, _)) => Ok(v2_supported),
+    //             Err(crate::Error::UnexpectedHttpStatus(_)) => Ok(false),
+    //             Err(e) => Err(e),
+    //         }
+    //     }
+    //
+    //     /// Check whether remote registry supports v2 API and `self` is authorized.
+    //     /// Authorized means to successfully GET the `/v2` endpoint on the remote registry.
+    //     pub async fn is_v2_supported_and_authorized(&self) -> Result<(bool, bool)> {
+    //         let api_header = "Docker-Distribution-API-Version";
+    //         let api_version = "registry/2.0";
+    //
+    //         // GET request to bare v2 endpoint.
+    //         let v2_endpoint = format!("{}/v2/", self.base_url);
+    //         let request = reqwest::Url::parse(&v2_endpoint).map(|url| {
+    //             trace!("GET {:?}", url);
+    //             self.build_reqwest(Method::GET, url)
+    //         })?;
+    //
+    //         let response = request.send().await?;
+    //
+    //         let b = match (response.status(), response.headers().get(api_header)) {
+    //             (StatusCode::OK, Some(x)) => Ok((x == api_version, true)),
+    //             (StatusCode::UNAUTHORIZED, Some(x)) => Ok((x == api_version, false)),
+    //             (s, v) => {
+    //                 trace!("Got unexpected status {}, header version {:?}", s, v);
+    //                 return Err(crate::Error::UnexpectedHttpStatus(s));
+    //             }
+    //         };
+    //
+    //         b
+    //     }
+    //
+    /// Takes reqwest's async RequestBuilder and injects an authentication header if a token is present
+
+    fn build_reqwest(
+        &self,
+        method: ::reqwest::Method,
+        url: reqwest::Url,
+    ) -> reqwest::blocking::RequestBuilder {
+        let mut builder = self.client.request(method, url);
+
+        if let Some(auth) = &self.auth {
+            builder = auth.add_auth_headers(builder);
+        };
+
+        if let Some(ua) = &self.user_agent {
+            builder = builder.header(reqwest::header::USER_AGENT, ua.as_str());
+        };
+
+        builder
+    }
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct ApiError {
+    code: String,
+    message: String,
+    detail: String,
+}
+
+#[derive(Debug, Default, Deserialize, Serialize)]
+struct Errors {
+    errors: Vec<ApiError>,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
